6452a4de4fca44f92a0121276b1af5d43707b96a
GabrielCastanhel <gabrielcastanhel2@gmail.com>
2025-06-06 20:41:42 -0300

    terminando o css da home

--- reforco/src/pages/Home.jsx
+++ reforco/src/pages/Home.jsx
@@ -3,8 +3,14 @@ import './Home.css'
 
 export default function Home() {
   return (
-    <div className='container-home'>
-      <Navbar />
-    </div>
+    <>
+      <div className='container-home'>
+        <Navbar />
+
+        <div className='home'>
+          <span className='title-home'>Reforço de JavaScript com o professor rafa</span>
+        </div>
+      </div>
+    </>
   )
 }
