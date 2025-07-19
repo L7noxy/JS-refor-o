ea2ccb9fc11ab106fc47807c2b74aab1ed905ea5
L7noxy <gabrielcastanhel2@gmail.com>
2025-07-18 21:34:20 -0300

    terminando por hoje

--- reforco/src/pages/Arrays.jsx
+++ reforco/src/pages/Arrays.jsx
@@ -2,37 +2,71 @@ import React from 'react'
 
 export default function Arrays() {
 
-    let number = ['ana', 'pedro', 'gabriel', 'nixolas']
+    //             0  1  2  3  4   5  6  
+    let numbers = [2, 5, 8, 2, 6, 7, 4]
+    //                0           1            2           3  
+    const names = ['Abgail', 'Astrogildo', 'Ursula', 'Ermenegildo']
+    //              0         1   2     3
+    let mix = ['Demóstenes', 12, 3.45, true]
 
-    let mix = ['MIX', 12, 200, true]
+    // ######### Métodos de Arrays #########
 
-    number.push(1)
+    // -> -> push(), pop(), unshift(), shift() <- <-
+    // numbers.push(1)
+    // console.log(numbers)
+    // numbers.pop()
+    // console.log(numbers)
 
-    number.slice(1)
+    // -> -> indexOf(), includes(), splice() <- <-
+    // console.log(names.indexOf('Ursula'))     //  2
+    // console.log(names.indexOf('Jurandir'))   // -1
+    // console.log(mix.includes('Demóstenes'))  // true
 
-    console.log(number)
+    // splice(posição, quantidade_deletar, o_que_incluir)
 
     let pos = 2
-
-    let posElement
-
-    let i
-
-    mix.splice(pos, 1 ,'janete')
+    mix.splice(pos, 1, 'Janete')
     console.log(mix)
 
-    for( i=0; i < number.length; i++){
+    //               0        1        2         3 
+    let mixDois = ['drum', 'angel', 'clown', 'guitar']
 
-        if ( number[i] === "gabriel" ){
-            posElement = i
+    let posElemento
+    //posElemento = mixDois.indexOf('clown')
+
+    for (i = 0; i < mixDois.length; i++) {
+
+        if (mixDois[i] == 'clown') {
+
+            posElemento = i
+            console.log(posElemento)
+
         }
+
     }
 
-    
 
-  return (
-    <div>
-      
-    </div>
-  )
+    const user = {
+
+        name: 'Abgail',
+        pass: 'biga123',
+        email: 'abiga_182@gmail.com',
+        phones: [],
+        newsletter: true
+
+    }
+
+    let vetorUsers = [
+        {}, // 0
+        {}, // 1
+        {}  // 2
+
+    ]
+
+    vetorUsers[0].pass = '666'
+    return (
+        <div>
+
+        </div>
+    )
 }
