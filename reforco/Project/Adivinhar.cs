19bc63a3cc91723643063b4ac4353320cbaae686
GABRIEL DA CUNHA CASTANHEL <gabriel_castanhel@estudante.sesisenai.org.br>
2025-08-15 20:32:45 -0300

    Colocando a logica em C#

---
+++ reforco/Project/Adivinhar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Adivinhar{
+    class Index{
+        public static void Main(string[] args)
+        {
+            Codigo();
+            Console.WriteLine("Pressione Enter para sair...");
+            Console.ReadLine();
+        }
+
+        public static void Codigo()
+        {
+            Random random = new Random();
+            int aleatorio = random.Next(1, 11);
+
+            Console.Write("Chute um número entre 1 e 10: ");
+            int palpite = Convert.ToInt32(Console.ReadLine()); 
+
+            if(aleatorio == palpite)
+            {
+                Console.WriteLine("ACERTOU o numero " + aleatorio + " e voce chutou " + palpite);
+            }
+            else
+            {
+                Console.Write("ERROU o numero era " + aleatorio + " e voce chutou " + palpite);
+            }
+        } 
+    }
+}
---
+++ reforco/Project/Arrays.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Arrays{
+    class Index{
+        public static void Main(string[] args)
+        {
+            Codigo();
+            Console.WriteLine("Pressione Enter para sair...");
+            Console.ReadLine();
+        }
+
+        public static void Codigo()
+        {
+            
+        }
+    }
+}
