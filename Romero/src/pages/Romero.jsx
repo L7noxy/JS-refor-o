aaf507c24b28e327db73ce4ea385c92ec630bba0
L7noxy <gabrielcastanhel2@gmail.com>
2025-08-29 21:18:01 -0300

    Colocando mais funcionalidades

--- Romero/src/pages/Romero.jsx
+++ Romero/src/pages/Romero.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Romero.css'
 
 export default function Romero() {
+
+  const produtos = [
+    {
+      id: 1,
+      nome: "teste1",
+      preco_promo: 0,
+      preco: 5,
+      antigo: true
+    },
+    {
+      id: 1,
+      nome: "teste1",
+      preco_promo: 0,
+      preco: 5,
+      antigo: false
+    },
+    {
+      id: 1,
+      nome: "teste1",
+      preco_promo: 0,
+      preco: 5,
+      antigo: true
+    },
+    {
+      id: 1,
+      nome: "teste1",
+      preco_promo: 0,
+      preco: 5,
+      antigo: false
+    },
+  ]
   return (
     <div>
-      ddd
+      <ul>
+        {produtos.map((produto) => (
+          <div key={produto.id}>
+            <div className='card-produto'>
+              <div>{produto.nome}</div>
+              <div>{produto.preco_promo}</div>
+              <div>{produto.preco}</div>
+              <div>{produto.antigo ? "Antigo" : "Novo"}</div>
+            </div>
+          </div>
+        ))}
+      </ul>
     </div>
   )
 }
